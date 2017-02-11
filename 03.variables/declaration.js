9f1528a1002337011f1e62af09b29913d5e1dde1
wangfei <aaron.wf@alibaba-inc.com>
2017-02-12 02:15:57 +0800

    enhance: add `03.variables` part

---
+++ 03.variables/declaration.js
@@ -0,0 +1,20 @@
+// 函数中定义变量
+function f() {
+    var msg = "Hello World";
+    return function g() {
+        var name = "Aaron Wong";
+        return msg + ', ' + name;
+    };
+}
+var g = f();
+// Hello world, Aaron Wong
+g();
+// 变量作用域
+function func(shouldInit) {
+    if (shouldInit) {
+        var a = 10;
+    }
+    return a;
+}
+func(true); // 10
+func(false); // undefined
