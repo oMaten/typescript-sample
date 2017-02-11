58293eab9df7fe2a2d81ccb0874a2213fa7af4e7
wangfei <aaron.wf@alibaba-inc.com>
2017-02-12 02:15:19 +0800

    enhance: add `01.start` part

---
+++ 01.start/index.js
@@ -0,0 +1,5 @@
+var user = "Aaron Wong";
+function greeter(person) {
+    return "Hello, " + person;
+}
+document.body.innerHTML = greeter(user);
---
+++ 01.start/index.ts
@@ -0,0 +1,7 @@
+var user = "Aaron Wong";
+
+function greeter (person: string) {
+  return "Hello, " + person;
+}
+
+document.body.innerHTML = greeter(user);
